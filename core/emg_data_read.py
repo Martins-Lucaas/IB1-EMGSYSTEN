790a28fd43d6df8b61d43d4b199732e6706f823b
renagod <94547891+renagod@users.noreply.github.com>
2024-01-02 15:10:22 -0300

    Mudei o ovo
    ovo

--- core/emg_data_read.py
+++ core/emg_data_read.py
@@ -6,7 +6,7 @@ from pathlib import Path
 # Carregar dados do arquivo Excel
 caminho_sinal = Path('IB1-EMGSYSTEN/dados/emg.xlsx')
 sinal_emg = pd.read_excel(caminho_sinal).values
-nome_canal = 'Mão Direita'
+nome_canal = 'ovo esquerdo'
 
 # Frequência de amostragem de 2000 (2000 amostras por segundo)
 frequencia_amostragem = 2e3
