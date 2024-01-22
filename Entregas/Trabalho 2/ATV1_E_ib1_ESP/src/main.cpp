a8611ffd4ba32548f353b027fe7a479ab25caee1
Lucas_Martins <lucaspmartins14@gmail.com>
2024-01-22 18:12:08 -0300

    changes

--- Entregas/Trabalho 2/ATV1_E_ib1_ESP/src/main.cpp
+++ Entregas/Trabalho 2/ATV1_E_ib1_ESP/src/main.cpp
@@ -1,48 +1,48 @@
 #include <Arduino.h>
 
 hw_timer_t *timer = NULL;
-const int intervaloMicrossegundos = 1000000;
+const int intervaloMicrossegundos = 100000;
 
-const int ledPins[] = {15, 2, 0, 4};
+const int ledPins[] = {2,30,31,32,33};
 const int numLeds = sizeof(ledPins) / sizeof(ledPins[0]);
 int currentLed = 0;
 
-const int potPin = 34;
+const int potPin = 4;
 volatile int valorPot = 0;
 volatile unsigned long previousMillis = 0;
 
 void IRAM_ATTR onTimer() {
-  valorPot = analogRead(potPin); // Read the potentiometer value
-  Serial.print("LED Frequency: ");
-  Serial.println(valorPot / 5);
+  valorPot = analogRead(potPin); // Executar a leitura do potenciômetro
 }
 
 void setup() {
   Serial.begin(115200);
 
-  // Initialize the timer
+  // Inicializar o temporizador
   timer = timerBegin(0, 80, true);
 
-  // Attach the interrupt function to the timer
+  // Anexar a funcao de interrupcao ao temporizador
   timerAttachInterrupt(timer, &onTimer, true);
 
-  // Set the timer frequency (reload interval)
+  // Definir a frequencia do temporizador (intervalo de recarga)
   timerAlarmWrite(timer, intervaloMicrossegundos, true);
 
-  // Enable the timer overflow interrupt
+  // Ativar a interrupção por estouro do temporizador
   timerAlarmEnable(timer);
 
   for (int i = 0; i < numLeds; i++) {
     pinMode(ledPins[i], OUTPUT);
+    Serial.print("Frequencia do LED: ");
+    Serial.println(valorPot/5);
   }
 }
 
 void loop() {
-  // Check if enough time has passed to switch to the next LED
-  if (millis() - previousMillis >= valorPot) {
+  // Verifica se tempo suficiente passou para mudar para o próximo LED
+  if (millis() - previousMillis >= valorPot)/5 {
     previousMillis = millis();
 
-    // Logic to control LEDs
+    // Lógica para acionar LEDs
     for (int i = 0; i < numLeds; i++) {
       digitalWrite(ledPins[i], LOW);
     }
@@ -50,5 +50,7 @@ void loop() {
     digitalWrite(ledPins[currentLed], HIGH);
 
     currentLed = (currentLed + 1) % numLeds;
+
+    
   }
 }
