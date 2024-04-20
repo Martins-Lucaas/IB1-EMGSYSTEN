70c5793685cd8f41d23c437faabf127af4ba8856
Lucas_Martins <lucaspmartins14@gmail.com>
2024-04-20 17:32:36 -0300

    Gráfico funcional 500hz

--- core/WebServer ESP-IOT/ESP_final/ESP_final.ino
+++ core/WebServer ESP-IOT/ESP_final/ESP_final.ino
@@ -1,272 +1,279 @@
 #include <WiFi.h>
 #include <WebServer.h>
+#include <Arduino.h>
 
-const char *ssid = "Net";
-const char *password = "12345678";
-hw_timer_t *My_timer = NULL;
+const char *ssid = "Martins WiFi6";
+const char *password = "17031998";
 
 WebServer server(80);
 
-bool updatingData = false; // Variável para controlar a atualização dos dados
+const int bufferSize = 250;
+float timeElapsed = 0;
+int emgBuffer[bufferSize];
+int bufferIndex = 0;
+bool updatingData = false;
+bool tableEnabled = false;
+unsigned long lastUpdateTime = 0;
+unsigned long lastReadTime = 0; // Variável para controlar o intervalo de leitura da resistência
+const unsigned long readInterval = 500; // Intervalo de leitura da resistência em milissegundos
+
+const int pinEMG = 34; // Porta do ESP conectada ao potenciômetro
+const int pinPot = 32;
+
+int readEMGValue() {
+  return analogRead(pinEMG);
+}
 
 void handleRoot() {
-  String html = 
-  "<!DOCTYPE HTML>"
-  "<html>"
-    "<head>"
+ String html =
+      "<!DOCTYPE HTML>"
+      "<html>"
+      "<head>"
       "<meta name='viewport' content='width=device-width, initial-scale=1'>"
       "<title>Projeto IB1</title>"
       "<script src='https://cdn.jsdelivr.net/npm/chart.js'></script>"
       "<style>"
-        "body {"
-          "font-family: Arial, sans-serif;"
-          "margin: 0;"
-          "padding: 0;"
-          "background-color: #f2f2f2;"
-        "}"
-        ".container {"
-          "max-width: 1500px;"
-          "margin: 50px auto;" // Centraliza o conteúdo na página
-          "padding: 20px;"
-          "background-color: #fff;"
-          "border-radius: 10px;"
-          "box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);"
-          "text-align: center;"
-        "}"
-        ".container h2 {"
-          "font-size: 24px;"
-          "color: #333;"
-        "}"
-        ".container p {"
-          "font-size: 18px;"
-          "color: #666;"
-        "}"
-        ".container canvas {"
-          "width: 100%;" // Permite que o gráfico se expanda horizontalmente
-          "height: 480px;"
-        "}"
-        ".container table {"
-          "margin-top: 20px;"
-          "border-collapse: collapse;"
-          "width: auto;"
-          "overflow-x: auto;" // Adiciona rolagem horizontal caso o conteúdo exceda a largura disponível
-          "margin-left: auto;" // Centraliza a tabela na página
-          "margin-right: auto;" // Centraliza a tabela na página
-        "}"
-        ".container th, .container td {"
-          "border: 1px solid #dddddd;"
-          "text-align: left;"
-          "padding: 8px;"
-          "min-width: 100px;" // Define uma largura mínima para cada célula da tabela
-        "}"
-        ".container th {"
-          "background-color: #f2f2f2;"
-        "}"
-        ".button {"
-          "border-radius: 20px;"
-          "padding: 10px 20px;"
-          "margin: 10px;"
-          "cursor: pointer;"
-          "border: none;"
-          "color: white;"
-          "font-size: 16px;"
-          "transition: background-color 0.2s ease;"
-        "}"
-        ".button.start[disabled] {"
-          "background-color: #ccc;"
-          "cursor: not-allowed;"
-        "}"
-        ".button.start {"
-          "background-color: green;"
-        "}"
-        ".button.start:hover {"
-          "background-color: darkgreen;"
-        "}"
-        ".button.start:active {"
-          "background-color: forestgreen;"
-        "}"
-        ".button.stop {"
-          "background-color: red;"
-        "}"
-        ".button.stop:hover {"
-          "background-color: darkred;"
-        "}"
-        ".button.stop:active {"
-          "background-color: firebrick;"
-        "}"
-        ".button.save {"
-          "background-color: blue;"
-        "}"
-        ".button.save:hover {"
-          "background-color: darkblue;"
-        "}"
-        ".button.save:active {"
-          "background-color: royalblue;"
-        "}"
-        ".button.table {"
-          "background-color: orange;"
-        "}"
-        ".button.table:hover {"
-          "background-color: darkorange;"
-        "}"
-        ".button.table:active {"
-          "background-color: orangered;"
-        "}"
-        ".hidden {"
-          "display: none;"
-        "}"
+      "body {"
+      "font-family: Arial, sans-serif;"
+      "margin: 0;"
+      "padding: 0;"
+      "background-color: #222;"
+      "color: #eee;"
+      "}"
+      ".container {"
+      "margin: 50px auto;"
+      "padding: 20px;"
+      "background-color: #333;"
+      "border-radius: 10px;"
+      "box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);"
+      "text-align: center;"
+      "}"
+      ".container h2 {"
+      "font-size: 32px;"
+      "color: #eee;"
+      "}"
+      ".container p {"
+      "font-size: 18px;"
+      "color: #ddd;"
+      "}"
+      ".container canvas {"
+      "width: 3000;" // Largura aumentada para três vezes a largura do contêiner
+      "height: 1000;"
+      "margin: 20px auto;"
+      "}"
+      ".button {"
+      "border-radius: 20px;"
+      "padding: 10px 20px;"
+      "margin: 10px;"
+      "cursor: pointer;"
+      "border: none;"
+      "color: #eee;"
+      "font-size: 16px;"
+      "transition: background-color 0.1s ease, transform 0.1s ease;"
+      "}"
+      ".button.start {"
+      "background-color: #4CAF50;"
+      "}"
+      ".button.start:hover {"
+      "background-color: #45a049;"
+      "}"
+      ".button.start:active {"
+      "transform: scale(0.95);"
+      "}"
+      ".button.stop {"
+      "background-color: #f44336;"
+      "}"
+      ".button.stop:hover {"
+      "background-color: #d32f2f;"
+      "}"
+      ".button.stop:active {"
+      "transform: scale(0.95);"
+      "}"
+      ".button.save {"
+      "background-color: #008CBA;"
+      "}"
+      ".button.save:hover {"
+      "background-color: #0077a3;"
+      "}"
+      ".button.save:active {"
+      "transform: scale(0.95);"
+      "}"
+      ".button.table {"
+      "background-color: #ff9800;"
+      "}"
+      ".button.table:hover {"
+      "background-color: #f57c00;"
+      "}"
+      ".button.table:active {"
+      "transform: scale(0.95);"
+      "}"
+      ".hidden {"
+      "display: none;"
+      "}"
       "</style>"
-    "</head>"
-    "<body>"
+      "</head>"
+      "<body>"
       "<div class='container'>"
-        "<h2>Projeto IB1</h2>"
-        "<p>Valor do Potenciometro: <span id='pot_value'>0</span></p>"
-        "<canvas id='potChart' width='1280' height='720'></canvas>"
-        "<button id='startButton' class='button start' onclick='start()'>Iniciar</button>"
-        "<button class='button stop' onclick='stop()'>Parar</button>"
-        "<button class='button save' onclick='save()'>Salvar</button>"
-        "<button id='showTableButton' class='button table' onclick='showTable()'>Ver Tabela</button>"
-        "<div id='tableContainer' class='hidden'></div>"
+      "<h2>Projeto IB1</h2>"
+      "<p>Valor do EMG: <span id='emg_value'>0</span></p>"
+      "<canvas id='potChart' width='3000' height='auto'></canvas>"
+      "<button id='startButton' class='button start' onclick='start()' disabled>Iniciar</button>"
+      "<button class='button stop' onclick='stop()'>Parar</button>"
+      "<button class='button save' onclick='save()'>Salvar</button>"
+      "<button id='showTableButton' class='button table' onclick='showTable()' disabled>Ver Tabela</button>"
+      "<div id='tableContainer' class='hidden'>"
+      "<table>"
+      "<thead>"
+      "<tr>"
+      "<th>Index</th>"
+      "<th>Value</th>"
+      "</tr>"
+      "</thead>"
+      "<tbody id='tableBody'></tbody>"
+      "</table>"
+      "</div>"
       "</div>"
       "<script>"
-        "var ctx = document.getElementById('potChart').getContext('2d');"
-        "var potChart = new Chart(ctx, {"
-          "type: 'line',"
-          "data: {"
-            "labels: [],"
-            "datasets: [{"
-              "label: 'Valor do Potenciometro',"
-              "data: [],"
-              "fill: false,"
-              "borderColor: 'rgb(75, 192, 192)',"
-            "}]"
-          "},"
-          "options: {"
-            "responsive: true,"
-            "scales: {"
-              "x: {"
-                "type: 'linear',"
-                "position: 'bottom'"
-              "},"
-              "y: {"
-                "suggestedMin: 0,"
-                "suggestedMax: 4095"
-              "}"
-            "}"
-          "}"
-        "});"
-        "var timeElapsed = 0;"
-        "var intervalId;"
-        "function updatePotChart(potValue) {"
-          "timeElapsed += 2;" // Incrementa o tempo decorrido por 0.2 (200 ms)
-          "potChart.data.labels.push(timeElapsed);" // Adiciona o tempo decorrido aos rótulos do eixo X
-          "potChart.data.datasets[0].data.push(potValue);" // Adiciona o valor do potenciômetro aos dados do gráfico
-          "potChart.update();" // Atualiza o gráfico
-        "}"
-        "function start() {"
-          "intervalId = setInterval(function() {"
-            "var xhttp = new XMLHttpRequest();"
-            "xhttp.onreadystatechange = function() {"
-              "if (this.readyState == 4 && this.status == 10) {"
-                "document.getElementById('pot_value').innerHTML = this.responseText;"
-                "updatePotChart(parseInt(this.responseText));" // Atualiza o gráfico com o valor do potenciômetro
-              "}"
-            "};"
-            "xhttp.open('GET', '/potValue', true);" // Envia uma solicitação para o servidor para obter o valor do potenciômetro
-            "xhttp.send();"
-          "}, 1);" // Iniciar atualização periódica a cada 200 ms
-          "document.getElementById('startButton').disabled = true;" // Desabilita o botão 'Iniciar'
-          "timeElapsed += 0.2;"
-          "potChart.data.labels.push(timeElapsed.toFixed(1));"
-          "potChart.data.datasets[0].data.push(potValue);"
-          "var chart = potChart;"
-          "var maxX = Math.max.apply(Math, chart.data.labels);"
-          "var minX = Math.min.apply(Math, chart.data.labels);"
-          "if (timeElapsed >= maxX) {"
-            "chart.options.scales.x.max = timeElapsed + 5;"
-            "chart.options.scales.x.min = minX;"
-            "chart.update();"
-          "} else {"
-            "potChart.update();"
-          "}"
-          "var tableHTML = '<table><tr><th>Tempo (s)</th><th>Valor do Potenciometro</th></tr>';"
-          "for (var i = 0; i < potChart.data.labels.length; i++) {"
-            "tableHTML += '<tr><td>' + potChart.data.labels[i] + '</td><td>' + potChart.data.datasets[0].data[i] + '</td></tr>';"
-          "}"
-          "tableHTML += '</table>';"
-          "document.getElementById('tableContainer').innerHTML = tableHTML;"
-        "}"
-        "function start() {"
-          "if (!updatingData) {"
-            "intervalId = setInterval(function() {"
-              "var xhttp = new XMLHttpRequest();"
-              "xhttp.onreadystatechange = function() {"
-                "if (this.readyState == 4 && this.status == 200) {"
-                  "document.getElementById('pot_value').innerHTML = this.responseText;"
-                  "updatePotChart(parseInt(this.responseText));"
-                "}"
-              "};"
-              "xhttp.open('GET', '/potValue', true);"
-              "xhttp.send();"
-            "}, 10);"
-            "document.getElementById('startButton').disabled = true;"
-            "updatingData = true;"
-          "}"
-        "}"
-        "function stop() {"
-          "clearInterval(intervalId);"
-          "document.getElementById('startButton').disabled = false;"
-          "updatingData = false;"
-        "}"
-        "function save() {"
-          "alert('Dados salvos!');"
-        "}"
-        "function showTable() {"
-          "var tableContainer = document.getElementById('tableContainer');"
-          "if (tableContainer.classList.contains('hidden')) {"
-            "tableContainer.classList.remove('hidden');"
-            "document.getElementById('showTableButton').innerText = 'Ocultar Tabela';"
-            "stop();"
-          "} else {"
-            "tableContainer.classList.add('hidden');"
-            "document.getElementById('showTableButton').innerText = 'Ver Tabela';"
-          "}"
-        "}"
+      "var ctx = document.getElementById('potChart').getContext('2d');"
+      "var potChart = new Chart(ctx, {"
+      "  type: 'line',"
+      "  data: {"
+      "    labels: [],"
+      "    datasets: [{"
+      "      label: 'Amplitude EMG',"
+      "      data: [],"
+      "      fill: false,"
+      "      borderColor: '#7FFFD4',"
+      "      backgroundColor: 'rgba(75, 192, 192, 0.2)',"
+      "      tension: 0.1"
+      "    }]"
+      "  },"
+      "  options: {"
+      "    animation: {"
+      "      duration: 0" // Desativa a animação
+      "    },"
+      "    scales: {"
+      "      x: {"
+      "        display: false,"
+      "      },"
+      "      y: {"
+      "        suggestedMin: 0,"
+      "        suggestedMax: 4095"
+      "      }"
+      "    }"
+      "  }"
+      "});"
+      "var timeElapsed = 0;"
+      "var intervalId;"
+      "var dataArray = [];"
+      "function updatePotChart(emgvalue) {"
+      "  dataArray.push({x: (timeElapsed * 250).toFixed(1), y: emgvalue});"
+      "  timeElapsed += 0.1;"
+      
+      "  if (dataArray.length > " + String(bufferSize) + ") {"
+      "    dataArray.shift();" // Remove o primeiro elemento do vetor
+      "    potChart.data.labels.shift();" // Remove o primeiro rótulo
+      "    potChart.data.datasets[0].data.shift();" // Remove o primeiro valor do conjunto de dados
+      "  }"
+      
+      "  potChart.data.labels.push(dataArray.length);"
+      "  potChart.data.datasets[0].data.push(emgvalue);"
+      "  potChart.update();"
+      "}"
+      "function start() {"
+      "  if (!updatingData) {"
+      "    intervalId = setInterval(function() {"
+      "      fetch('/emgvalue')"
+      "      .then(response => response.text())"
+      "      .then(data => {"
+      "        document.getElementById('emg_value').innerText = data;"
+      "        updatePotChart(parseInt(data));"
+      "      });"
+      "    }, 1);"
+      "    document.getElementById('startButton').disabled = true;"
+      "    document.getElementById('showTableButton').disabled = false;"
+      "    updatingData = true;"
+      "  }"
+      "}"
+      "function stop() {"
+      "  clearInterval(intervalId);"
+      "  document.getElementById('startButton').disabled = false;"
+      "  updatingData = false;"
+      "}"
+      "function save() {"
+      "  alert('Dados salvos!');"
+      "}"
+      "function showTable() {"
+      "  var tableContainer = document.getElementById('tableContainer');"
+      "  if (tableContainer.classList.contains('hidden')) {"
+      "    tableContainer.classList.remove('hidden');"
+      "    document.getElementById('showTableButton').innerText = 'Ocultar Tabela';"
+      "    fillTable();"
+      "  } else {"
+      "    tableContainer.classList.add('hidden');"
+      "    document.getElementById('showTableButton').innerText = 'Ver Tabela';"
+      "  }"
+      "}"
+      "function fillTable() {"
+      "  var tableBody = document.getElementById('tableBody');"
+      "  tableBody.innerHTML = '';"
+      "  dataArray.forEach(function(data, index) {"
+      "    var row = tableBody.insertRow();"
+      "    var indexCell = row.insertCell(0);"
+      "    var valueCell = row.insertCell(1);"
+      "    indexCell.innerText = index + 1;"
+      "    valueCell.innerText = data.y;"
+      "  });"
+      "}"
       "</script>"
-    "</body>"
-  "</html>";
-  server.send(10, "text/html", html);
-
+      "</body>"
+      "</html>";
   server.send(200, "text/html", html);
 }
 
 void setup() {
+  pinMode(pinEMG, INPUT);
+  pinMode(pinPot, INPUT);
   Serial.begin(115200);
 
-  // Conectar-se à rede Wi-Fi
   WiFi.begin(ssid, password);
-  
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.println("Conectando ao WiFi...");
+    Serial.println("Connecting to WiFi...");
   }
 
-  // Impressão do endereço IP após a conexão bem sucedida
-  Serial.println("Conectado à rede WiFi");
-  Serial.print("Endereço IP: ");
+  Serial.println("Connected to WiFi");
+  Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
 
-  // Definir as rotas
   server.on("/", HTTP_GET, handleRoot);
-  server.on("/potValue", HTTP_GET, []() {server.send(10, "text/plain", String(analogRead(34)));
+  server.on("/emgvalue", HTTP_GET, []() {
+    server.send(200, "text/plain", String(readEMGValue()));
   });
 
-  // Iniciar o servidor
   server.begin();
 
-  Serial.println("Servidor iniciado");
+  pinMode(32, INPUT);
+
+  Serial.println("Server started");
 }
 
 void loop() {
-  server.handleClient(); // Lidar com as solicitações dos clientes
+  server.handleClient();
+  
+  if (millis() - lastUpdateTime >= 1) {
+    lastUpdateTime = millis();
+    if (updatingData) {
+      int emgvalue = readEMGValue();
+      server.send(200, "text/plain", String(emgvalue));
+    }
+  }
+
+  // Intervalo para leitura da resistência a cada 0.5 segundo
+  if (millis() - lastReadTime >= readInterval) {
+    lastReadTime = millis();
+    int valorADC = analogRead(pinPot); // Lê o valor analógico na porta do potenciômetro
+    float resistencia = map(valorADC, 0, 4095, 0, 200000); // Mapeia o valor ADC para um intervalo de resistência
+    Serial.println(resistencia); // Imprime a resistência do potenciômetro
+  }
 }
